d6ba77ad71b58c81961d575e293829feec580bab
sasuske <jelver00@hotmail.com>
2022-08-10 10:27:36 -0500

    first commit

---
+++ src/components/ResidentsList.jsx
@@ -0,0 +1,73 @@
+import '../styles/ResidentsList.css';
+import React, { useEffect, useState } from 'react';
+import ResidentInfo from './ResidentInfo';
+import Pagination from './Pagination';
+import Loading from './Loading';
+import { DEFAULT_APP_CONFIG } from '../sample/config';
+
+const initialPaginationInfo = {
+   page: 1,
+   offset: 0,
+   elementsPerPage: DEFAULT_APP_CONFIG.elementsPerPage,
+};
+
+const ResidentsList = ({ residents, error, executeScroll }) => {
+   const [pagination, setPagination] = useState(initialPaginationInfo);
+   const pages = residents
+      ? Math.ceil(residents.length / DEFAULT_APP_CONFIG.elementsPerPage)
+      : 0;
+
+   useEffect(() => {
+      setPagination(initialPaginationInfo);
+   }, [residents]);
+
+   return (
+      <div className='ResidentListComponent'>
+         <h2 className='residents-title'>Residents</h2>
+         <hr className='hr residents-hr' />
+         {residents?.length === 0 && (
+            <div className='no-data-message'>
+               There are no residents to display...
+            </div>
+         )}
+         {residents ? (
+            <>
+               <div className='residents-container'>
+                  {residents
+                     .slice(
+                        pagination.offset,
+                        pagination.offset + pagination.elementsPerPage
+                     )
+                     .map((resident) => (
+                        <ResidentInfo
+                           key={resident.match(/(\d)+$/)[0]}
+                           resident={resident}
+                        />
+                     ))}
+               </div>
+               <div className='pagination-section'>
+                  {pages > 1
+                     ? Array.from(Array(pages).keys(), (x) => x + 1).map(
+                          (index) => (
+                             <Pagination
+                                key={index}
+                                page={index}
+                                elementsPerPage={pagination.elementsPerPage}
+                                setPagination={setPagination}
+                                executeScroll={executeScroll}
+                             />
+                          )
+                       )
+                     : null}
+               </div>
+            </>
+         ) : error?.error ? (
+            <div className='error-message'>Something went wrong...</div>
+         ) : (
+            <Loading />
+         )}
+      </div>
+   );
+};
+
+export default ResidentsList;
